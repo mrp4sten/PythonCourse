06a723aa2361448aa158081c933bd4b6734a3907
Mauricio Pasten <mauricio.pasten.martinez@gmail.com>
2022-12-09 23:35:42 -0600

    First Python script

---
+++ firstProgram.py
@@ -0,0 +1,9 @@
+number_one = int(input("Enter number one: "))
+number_two = int(input("Enter number two: "))
+number_three = int(input("Enter number three: "))
+
+min_number = min(number_one, number_two, number_three)
+max_number = max(number_one, number_two, number_three)
+
+print("Max number: " + str(max_number))
+print("Min number: " + str(min_number))
