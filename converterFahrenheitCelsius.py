14e07baddaa90ae1b13f66d34bcb02ce3cb4b00b
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-12-17 20:24:08 -0600

    First converters

---
+++ converterFahrenheitCelsius.py
@@ -0,0 +1,6 @@
+# Equation: (F - 32)(5/9)=C
+
+fahrenheit = float(input("Enter your fahrenheit degrees: "))
+celsius = (fahrenheit - 32) * (5/9)
+
+print("{}°F is equal to {}°C".format(fahrenheit, celsius))
---
+++ converterPoundsEuro.py
@@ -0,0 +1,6 @@
+# Equation: £ * 1.5
+
+pounds = float(input("Enter your Pounds(£): "))
+euro = pounds * 1.5
+
+print("{}£ is equal to {}€".format(pounds, euro))
