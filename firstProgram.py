ff6307143b19052c29685b0922d846c60a0f1bd8
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-12-17 20:20:22 -0600

    Using format method

--- firstProgram.py
+++ firstProgram.py
@@ -2,8 +2,5 @@ number_one = int(input("Enter number one: "))
 number_two = int(input("Enter number two: "))
 number_three = int(input("Enter number three: "))
 
-min_number = min(number_one, number_two, number_three)
-max_number = max(number_one, number_two, number_three)
-
-print("Max number: " + str(max_number))
-print("Min number: " + str(min_number))
+print("Max number: {}".format(max(number_one, number_two, number_three)))
+print("Min number: {}".format(min(number_one, number_two, number_three)))
